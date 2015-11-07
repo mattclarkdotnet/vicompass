fcea30067790ef57c8b4da49a4aa559851510ada
Matt Clark <matt@effra.local>
2015-11-07 18:31:06 +0000

    Set app to ignore idle timer, and therefore not allow sleep when in use

--- VISCompass/AppDelegate.swift
+++ VISCompass/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         log.setup(.Debug, showLogLevel: true, showFileNames: true, showLineNumbers: true, writeToFile: nil)
+        application.idleTimerDisabled = true
         return true
     }
 
